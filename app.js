6bd5d012f095273ab2f8e0d18b9ec6955e6a1609
Jaarabytes <128234882+Jaarabytes@users.noreply.github.com>
2023-05-24 12:43:55 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    var container = $("#main");
+    $(container).animate({
+        opacity:1
+    }, 2000)
+    $("#details").animate({
+        opacity:1
+    },2000)   
+})
